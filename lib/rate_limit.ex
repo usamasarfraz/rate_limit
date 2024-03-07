4d601bd4060507dfe973c89a9044e0cc1c6b9990
usamasarfraz <usamasarfraz822@gmail.com>
2024-03-07 13:37:22 +0500

    adding update scale and limit by admin

--- lib/rate_limit.ex
+++ lib/rate_limit.ex
@@ -3,34 +3,51 @@ defmodule RateLimit do
   alias RateLimit.Utils
   alias RateLimit.DynamicSupervisor
 
-  @spec check_rate(id :: String.t(), scale_ms :: integer, limit :: integer) ::
+  @spec check_rate(id :: String.t()) ::
   {:allow, count :: integer}
   | {:deny, limit :: integer}
   | {:error, reason :: any}
 
-  def check_rate(id, scale_ms, limit) do
-    case call_worker(id, scale_ms) do
-      {:ok, count} ->
+  def check_rate(id) do
+    new_id = String.to_atom(id)
+    case call_worker(new_id, 60000) do
+      {:ok, count, limit} ->
         if count > limit do
           {:deny, limit}
         else
           {:allow, count}
         end
-
       {:error, reason} ->
         {:error, reason}
     end
   end
 
+  def update_rate(id, scale_ms, limit) do
+    new_id = String.to_atom(id)
+    Worker.update_limit_and_scale(new_id, scale_ms, limit)
+  end
+
+  def update_scale(id, scale_ms) do
+    new_id = String.to_atom(id)
+    Worker.update_scale(new_id, scale_ms)
+  end
+
+  def update_limit(id, limit) do
+    new_id = String.to_atom(id)
+    Worker.update_limit(new_id, limit)
+  end
+
   defp call_worker(id, scale_ms) do
-    args = %{"count_hit" => 1,"created_at" => Utils.timestamp,"updated_at" => Utils.timestamp,"last_seconds_hit" => 0,"id" => String.to_atom(id),"scale_ms" => scale_ms,"expire" => Utils.timestamp + scale_ms}
+    args = %{"count_hit" => 1,"limit" => 10,"created_at" => Utils.timestamp,"updated_at" => Utils.timestamp,"last_seconds_hit" => 0,"id" => id,"scale_ms" => scale_ms,"expire" => Utils.timestamp + scale_ms}
     DynamicSupervisor.start_child(args)
     |> case do
       {:ok, _pid} ->
-        {:ok, 1}
+        {:ok, 1, 10}
       {:error, {:already_started, pid}} ->
-        %{"count_hit" => count_hit} = Worker.count_hit(pid)
-        {:ok, count_hit}
+        %{"count_hit" => count_hit, "limit" => limit} = Worker.count_hit(pid)
+        {:ok, count_hit, limit}
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 end
--- lib/rate_limit/worker.ex
+++ lib/rate_limit/worker.ex
@@ -15,6 +15,18 @@ defmodule RateLimit.Worker do
     GenServer.call(pid, {:count_hit})
   end
 
+  def update_limit_and_scale(id, scale_ms, limit) do
+    GenServer.call(id, {:update_limit_and_scale, scale_ms, limit})
+  end
+
+  def update_limit(id, limit) do
+    GenServer.call(id, {:update_limit, limit})
+  end
+
+  def update_scale(id, scale_ms) do
+    GenServer.call(id, {:update_scale, scale_ms})
+  end
+
   def handle_call({:count_hit}, _from, %{"expire" => expire_at, "count_hit" => count_hit, "last_seconds_hit" => last_seconds_hit} = state) do
     if expire_at - Utils.timestamp <= 15000 do
       updated_state = %{state | "count_hit" => count_hit + 1, "last_seconds_hit" => last_seconds_hit + 1}
@@ -23,6 +35,21 @@ defmodule RateLimit.Worker do
       updated_state = %{state | "count_hit" => count_hit + 1}
       {:reply, updated_state, updated_state}
     end
+  end
+
+  def handle_call({:update_limit_and_scale, scale_ms, limit}, _from, state) do
+    updated_state = %{state | "limit" => limit, "scale_ms" => scale_ms}
+    {:reply, updated_state, updated_state}
+  end
+
+  def handle_call({:update_limit, limit}, _from, state) do
+    updated_state = %{state | "limit" => limit}
+    {:reply, updated_state, updated_state}
+  end
+
+  def handle_call({:update_scale, scale_ms}, _from, state) do
+    updated_state = %{state | "scale_ms" => scale_ms}
+    {:reply, updated_state, updated_state}
   end
 
   def handle_info({:interval, time_ms}, state) do
