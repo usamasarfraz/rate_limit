1e5fe3ca31e87b2bed1ab7ac4cf1dd8b22d8c97f
usamasarfraz <usamasarfraz822@gmail.com>
2024-03-07 14:09:47 +0500

    adding spec

--- lib/rate_limit.ex
+++ lib/rate_limit.ex
@@ -24,19 +24,34 @@ defmodule RateLimit do
     end
   end
 
+  @spec update_rate(id :: String.t(), scale_ms :: integer, limit :: integer) ::
+  {:ok, limit :: integer, scale_ms :: integer}
+
   def update_rate(id, scale_ms, limit) do
+    %{"limit" => limit, "scale_ms" => scale_ms} =
     String.to_atom(id)
     |> Worker.update_limit_and_scale(scale_ms, limit)
+    {:ok, limit, scale_ms}
   end
+
+  @spec update_scale(id :: String.t(), scale_ms :: integer) ::
+  {:ok, scale_ms :: integer}
 
   def update_scale(id, scale_ms) do
+    %{"scale_ms" => scale_ms} =
     String.to_atom(id)
     |> Worker.update_scale(scale_ms)
+    {:ok, scale_ms}
   end
 
+  @spec update_limit(id :: String.t(), limit :: integer) ::
+  {:ok, limit :: integer}
+
   def update_limit(id, limit) do
+    %{"limit" => limit} =
     String.to_atom(id)
     |> Worker.update_limit(limit)
+    {:ok, limit}
   end
 
   defp call_worker(id, scale_ms) do
